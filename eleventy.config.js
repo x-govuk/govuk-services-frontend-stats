3187ca825fc181dedf1ef8c19cbb1edd102a1fc6
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2025-05-21 00:24:09 +0100

    Implement X-GOVUK rebrand (#27)
    Implements X-GOVUK rebrand (aka GOV.UK rebrand with X-GOVUK design
    tweaks):
    
    - Update header
      - Update logo 
      - Update theme colour to match header
    - Update links to icon assets
    - Add link to generic X-GOVUK Open Graph image
    - Add X-GOVUK crown to footer

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,22 +1,26 @@
-import govukEleventyPlugin from "@x-govuk/govuk-eleventy-plugin";
+import { govukEleventyPlugin } from "@x-govuk/govuk-eleventy-plugin";
 
 export default function (eleventyConfig) {
   // Register the plugin
   eleventyConfig.addPlugin(govukEleventyPlugin, {
     icons: {
-      mask: "https://raw.githubusercontent.com/x-govuk/logo/main/images/x-govuk-mask-icon.svg?raw=true",
+      mask: "https://raw.githubusercontent.com/x-govuk/logo/main/images/x-govuk-icon-mask.svg?raw=true",
       shortcut:
-        "https://raw.githubusercontent.com/x-govuk/logo/main/images/x-govuk-favicon.ico",
+        "https://raw.githubusercontent.com/x-govuk/logo/main/images/favicon.ico",
       touch:
-        "https://raw.githubusercontent.com/x-govuk/logo/main/images/x-govuk-apple-touch-icon.png",
+        "https://raw.githubusercontent.com/x-govuk/logo/main/images/x-govuk-icon-180.png",
     },
+    opengraphImageUrl:
+      "https://raw.githubusercontent.com/x-govuk/logo/main/images/x-govuk-opengraph-image.png",
+    themeColor: "#2288aa",
+    titleSuffix: "X-GOVUK",
     url: process.env.GITHUB_ACTIONS
       ? "https://x-govuk.github.io/govuk-rspec-helpers/"
       : "/",
+    stylesheets: ["/assets/application.css"],
     header: {
-      logotype: "x-govuk",
+      homepageUrl: "https://x-govuk.github.io",
     },
-    titleSuffix: "X-GOVUK",
     footer: {
       contentLicence: {
         html: 'Licensed under the <a class="govuk-footer__link" href="https://github.com/x-govuk/govuk-prototype-components/blob/main/LICENSE.txt">MIT Licence</a>, except where otherwise stated',
@@ -25,19 +29,19 @@ export default function (eleventyConfig) {
         text: "© X-GOVUK",
       },
     },
+    rebrand: true,
   });
 
   // Passthrough
   eleventyConfig.addPassthroughCopy("./assets");
 
+  // Enable X-GOVUK brand
+  eleventyConfig.addNunjucksGlobal("xGovuk", true);
+
   return {
     dataTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk",
-    dir: {
-      // Use layouts from the plugin
-      layouts: "node_modules/@x-govuk/govuk-eleventy-plugin/layouts",
-    },
     pathPrefix: process.env.GITHUB_ACTIONS
       ? "/govuk-services-frontend-stats/"
       : "/",
