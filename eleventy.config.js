64898ddb5cc742c54d81939d773bf88225266374
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2025-08-26 23:57:39 +0100

    Order plugin options alphabetically

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,17 @@ import { govukEleventyPlugin } from "@x-govuk/govuk-eleventy-plugin";
 export default function (eleventyConfig) {
   // Register the plugin
   eleventyConfig.addPlugin(govukEleventyPlugin, {
+    footer: {
+      contentLicence: {
+        html: 'Licensed under the <a class="govuk-footer__link" href="https://github.com/x-govuk/govuk-prototype-components/blob/main/LICENSE.txt">MIT Licence</a>, except where otherwise stated',
+      },
+      copyright: {
+        text: "© X-GOVUK",
+      },
+    },
+    header: {
+      homepageUrl: "https://x-govuk.github.io",
+    },
     icons: {
       mask: "https://raw.githubusercontent.com/x-govuk/logo/main/images/x-govuk-icon-mask.svg?raw=true",
       shortcut:
@@ -12,24 +23,12 @@ export default function (eleventyConfig) {
     },
     opengraphImageUrl:
       "https://raw.githubusercontent.com/x-govuk/logo/main/images/x-govuk-opengraph-image.png",
+    stylesheets: ["/assets/application.css"],
     themeColor: "#2288aa",
     titleSuffix: "X-GOVUK",
     url: process.env.GITHUB_ACTIONS
       ? "https://x-govuk.github.io/govuk-services-frontend-stats/"
       : "/",
-    stylesheets: ["/assets/application.css"],
-    header: {
-      homepageUrl: "https://x-govuk.github.io",
-    },
-    footer: {
-      contentLicence: {
-        html: 'Licensed under the <a class="govuk-footer__link" href="https://github.com/x-govuk/govuk-prototype-components/blob/main/LICENSE.txt">MIT Licence</a>, except where otherwise stated',
-      },
-      copyright: {
-        text: "© X-GOVUK",
-      },
-    },
-    rebrand: true,
   });
 
   // Passthrough
